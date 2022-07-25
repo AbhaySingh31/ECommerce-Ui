6aadcbd28fcf2e411c581e5b9fdf8b2955fd649a
Abhay Singh <abhay.singh.01432@gmail.com>
2022-07-26 00:06:03 +0530

    to test protected branch

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -62,7 +62,7 @@ const Login = () => {
           <Input placeholder="name" />
           <Input placeholder="Password" />
           <Button>LOGIN</Button>
-          <Link>FORGET PASSWORD? from 31yoyoy</Link>
+          <Link>FORGET PASSWORD? from 31yoyoy to test</Link>
           <Link>CREATE A NEW ACCOUNT</Link>
         </Form>
       </Wrapper>
